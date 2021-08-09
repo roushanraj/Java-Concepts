061661bcff8f8149b6506b62758c8c969f015c36
Roushan Raj <roushanrj776@gmail.com>
2021-08-09 10:54:09 +0530

    String Operation Update

--- SampleProject/src/StringOperationAssignment.java
+++ SampleProject/src/StringOperationAssignment.java
@@ -13,7 +13,7 @@ public class StringOperationAssignment {
 	/**
 	 * <h3>String operation method</h3>
 	 * 
-	 * This method takes input of string with even length.
+	 * This method takes input of string.
 	 *  And divide the String into two equal halves and shows the console output where 
 	 * second half is printed first and second half is reversed and then printed later.<br>
 	 * <b>Example</b> ABCDEF--> ABC DEF--> DEF CBA--> DEFCBA <br>
@@ -24,14 +24,21 @@ public class StringOperationAssignment {
 	 * @author Roushan Raj
 	 */
 	private static void stringOperation(String text) {
-		if(text.length()%2!=0) {
-			System.out.println("--Please Input Even No of Characters--");
-			System.exit(0);
+		int flag=0;
+		char mid = 0;
+		if (text.length()%2!=0) {
+			flag=1;
+			mid= text.charAt(text.length()/2);
+			StringBuilder temp= new StringBuilder(text);
+			temp.deleteCharAt(text.length()/2);
+			text=temp.toString();
+			
 		}
 		
-		String second= text.substring(text.length()/2, text.length());
+		String second= text.substring((text.length()/2), text.length());
 		System.out.print(second);
-
+		if(flag==1)
+			System.out.print(mid);
 		text=text.substring(0, text.length()/2);
 		
 		System.out.println(reverse(text));
